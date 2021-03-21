78e9a0696979dca3358697046da7b4a0c29ff7bd
saharsa <saharsamohanty@gmail.com>
2021-03-22 01:32:35 +0530

    Script changes

--- bin/parking_lot.sh
+++ bin/parking_lot.sh
@@ -1,1 +1,5 @@
-java -cp ../target/classes/ com.saharsa.App
+#!/usr/bin/bash
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+TARGETPATH=`echo $SCRIPTPATH|sed -e 's/bin/target/'`
+java -jar $TARGETPATH/parking_lot-1.0-SNAPSHOT.jar $1 $2 $3
--- bin/setup.sh
+++ bin/setup.sh
@@ -1,1 +1,5 @@
-mvn -f ../pom.xml clean install
+#!/usr/bin/bash
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+CODEPATH=$(dirname "$SCRIPTPATH")
+mvn -f $CODEPATH/pom.xml clean install

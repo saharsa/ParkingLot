8221afbd38cafab351eac80c3377e7c53e12e3ee
saharsa <saharsamohanty@gmail.com>
2021-03-23 13:09:37 +0530

    Adding relevant comments in ParkingLot

--- src/main/java/com/saharsa/App.java
+++ src/main/java/com/saharsa/App.java
@@ -8,16 +8,15 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
 public class App {
 
-    public static ParkingLot parkingLot = null;
+    public static ParkingLot parkingLot;
+
+    static {
+        parkingLot = new ParkingLot();
+    }
 
     public static void main(String[] args) throws IOException {
-
 
         if(args!=null && args.length>0 && args[0]!=null){
             Path path = Paths.get(args[0]);
@@ -25,47 +24,6 @@ public class App {
         }else{
             runInteractiveShell();
         }
-
-        /*try {
-            ParkingLot parkingLot = new ParkingLot(6);
-
-            Vehicle car = new Vehicle("KA-01-HH-1234", "White");
-            parkingLot.park(car);
-
-            Vehicle car1 = new Vehicle("KA-01-HH-9999", "White");
-            parkingLot.park(car1);
-
-            Vehicle car2 = new Vehicle("KA-01-BB-0001", "Black");
-            parkingLot.park(car2);
-
-            Vehicle car3 = new Vehicle("KA-01-HH-7777", "Red");
-            parkingLot.park(car3);
-
-            Vehicle car4 = new Vehicle("KA-01-HH-2701", "Blue");
-            parkingLot.park(car4);
-
-            Vehicle car5 = new Vehicle("KA-01-HH-3141", "Black");
-            parkingLot.park(car5);
-
-            Vehicle car6 = new Vehicle("KA-01-P-333", "White");
-            parkingLot.park(car6);
-
-            parkingLot.leave(4);
-            parkingLot.leave(1);
-
-            parkingLot.status();
-
-            Vehicle car7 = new Vehicle("DL-12-AA-9999", "White");
-            parkingLot.park(car7);
-
-            System.out.println(parkingLot.getSlotByRegistrationNumber("KA-01-HH-7777"));
-
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
-         */
 
     }
 
@@ -102,35 +60,28 @@ public class App {
                     exit = true;
                     break;
                 case "create_parking_lot":
-                    ParkingLot.createParkingLot(Integer.parseInt(arguments[0]));
-                    parkingLot = ParkingLot.getParkingLot();
+                    parkingLot.createParkingLot(Integer.parseInt(arguments[0]));
                     break;
                 case "park":
                     Vehicle vehicle = new Vehicle(arguments[0], arguments[1]);
-                    assert parkingLot != null;
                     parkingLot.park(vehicle);
                     break;
                 case "leave":
-                    assert parkingLot != null;
                     parkingLot.leave(Integer.parseInt(arguments[0]));
                     break;
                 case "status":
-                    assert parkingLot != null;
                     parkingLot.status();
                     break;
                 case "slot_number_for_registration_number":
-                    assert parkingLot != null;
-                    System.out.println(parkingLot.getSlotByRegistrationNumber(arguments[0]));
+                    System.out.println(parkingLot.getSpotByRegistrationNumber(arguments[0]));
                     break;
                 case "registration_numbers_for_cars_with_colour":
-                    assert parkingLot != null;
                     List<String> list = parkingLot.getRegistrationNumbersByColor(arguments[0]);
                     list.forEach((l) -> System.out.print(l+", "));
                     System.out.println();
                     break;
                 case "slot_numbers_for_cars_with_colour":
-                    assert parkingLot != null;
-                    List<Integer> list1 = parkingLot.getSlotNumbersByColor(arguments[0]);
+                    List<Integer> list1 = parkingLot.getSpotNumbersByColor(arguments[0]);
                     list1.forEach((l) -> System.out.print(l+", "));
                     System.out.println();
                     break;
--- src/main/java/com/saharsa/ParkingLot.java
+++ src/main/java/com/saharsa/ParkingLot.java
@@ -5,55 +5,128 @@ import java.util.stream.Collectors;
 
 public class ParkingLot {
 
-    private static ParkingLot parkingLot;
-
+    // Checks if ParkingLot instance has been specifically created by the user using create_parking_lot command
+    private boolean created = false;
+    // Map of occupied spots in the ParkingLot
     private Map<Integer, ParkingSpot> occupiedSpots;
-    private Queue<ParkingSpot> emptySpots;
+    // A priority queue of empty spot IDs
+    private Queue<Integer> emptySpots;
+    // Max capacity of ParkingLot
+    private int capacity;
 
-    public static ParkingLot getParkingLot() {
-        return parkingLot;
+    public Map<Integer, ParkingSpot> getOccupiedSpots() {
+        return this.occupiedSpots;
     }
 
-    public static void createParkingLot(int totalSpots) throws Exception {
-        if(parkingLot == null) {
-            parkingLot = new ParkingLot(totalSpots);
+    public Queue<Integer> getEmptySpots() {
+        return this.emptySpots;
+    }
+
+    /**
+     * Creates a ParkingLot with the specified capacity.
+     * Initializes the ParkingLot instance and sets created to true.
+     * @param totalSpots Total capacity of parking lot
+     * @throws Exception Throws exception if parking lot is already created
+     */
+    public void createParkingLot(int totalSpots) throws Exception {
+        if(!created) {
+            this.initializeParkingLot(totalSpots);
+            System.out.println("Parking lot created");
             return;
         }
         throw new Exception("Parking lot already created");
 
     }
 
-    private ParkingLot(int totalSpots) {
+    /**
+     * Initializes the ParkingLot instance with the capacity and initializes the relevant maps and queues.
+     * @param totalSpots Total capacity of parking lot
+     */
+    private void initializeParkingLot(int totalSpots) {
         this.occupiedSpots = new HashMap<>(totalSpots);
         this.emptySpots = new PriorityQueue<>(totalSpots);
         for(int i=0; i<totalSpots; i++) {
-            this.emptySpots.add(new ParkingSpot(i+1));
+            this.emptySpots.add(i+1);
         }
-        System.out.println("Parking lot created");
+        this.created = true;
+        this.capacity = totalSpots;
     }
 
-    public void park(Vehicle vehicle) throws Exception {
-        ParkingSpot parkingSpot = getNextAvailableSpot();
+    /**
+     * Before execution of any command, checks if ParkingLot is indeed created by the user or not.
+     * @throws Exception Throws exception if ParkingLot not created by user yet.
+     */
+    private void checkParkingLot() throws Exception {
+        if(!created) {
+            throw new Exception("Parking lot not created yet");
+        }
+    }
+
+    /**
+     * Parks the vehicle at the closest available spot in the ParkingLot
+     * @param vehicle Vehicle to be parked
+     * @throws Exception Throws exception if parking lot not created or parking lot is full
+     */
+    public ParkingSpot park(Vehicle vehicle) throws Exception {
+
+        checkParkingLot();
+
+        Integer spotId = useNextAvailableSpot();
+        ParkingSpot parkingSpot = new ParkingSpot(spotId);
         parkingSpot.assignVehicle(vehicle);
-        occupiedSpots.put(parkingSpot.getSpotId(), parkingSpot);
-        System.out.println("Allocated slot number: "+parkingSpot.getSpotId());
+        occupiedSpots.put(spotId, parkingSpot);
+        System.out.println("Allocated slot number: "+spotId);
+        return parkingSpot;
     }
 
-    public void leave(int spotId){
+    /**
+     * Removes a Vehicle from a ParkingSpot when it leaves the ParkingLot
+     * @param spotId ID of the spot where the Vehicle is parked
+     * @throws Exception Throws exception if parking lot not created, or parking spot unoccupied, or SpotID invalid
+     */
+    public ParkingSpot leave(int spotId) throws Exception {
+
+        checkParkingLot();
+
+        if(spotId > capacity) {
+            throw new Exception("Such a parking spot does not exist");
+        }
+
         ParkingSpot parkingSpot = occupiedSpots.remove(spotId);
+
+        if(parkingSpot == null) {
+            throw new Exception("Parking spot unoccupied");
+        }
+
         parkingSpot.removeVehicle();
         freeSpot(parkingSpot);
         System.out.println("Slot number "+parkingSpot.getSpotId()+" is free");
+
+        return parkingSpot;
     }
 
-    public void status(){
+    /**
+     * Gives a summary of the ParkingLot's occupied ParkingSpot's
+     * @throws Exception Throws exception if ParkingLot not created
+     */
+    public void status() throws Exception {
+
+        checkParkingLot();
+
         System.out.println("Slot No.\tRegistration No.\t\tColor");
-        this.occupiedSpots.forEach((spotId, spot) -> {
-            System.out.println(spotId+"\t\t\t"+spot.getVehicle().getRegistrationNumber()+"\t\t\t"+spot.getVehicle().getColor());
-        });
+        this.occupiedSpots.forEach((spotId, spot) -> System.out.println(spotId+"\t\t\t"+spot.getVehicle().getRegistrationNumber()+"\t\t\t"+spot.getVehicle().getColor()));
     }
 
-    public Integer getSlotByRegistrationNumber(String registrationNumber) throws Exception {
+    /**
+     * Returns SpotID by registration number of Vehicle
+     * @param registrationNumber Registration number of Vehicle
+     * @return SpotID
+     * @throws Exception Throws exception if parking lot not created or vehicle not found
+     */
+    public Integer getSpotByRegistrationNumber(String registrationNumber) throws Exception {
+
+        checkParkingLot();
+
         Optional<ParkingSpot> spot = this.occupiedSpots.values().stream().filter((e) -> e.getVehicle().getRegistrationNumber().equals(registrationNumber)).findFirst();
         if(spot.isPresent()) {
             return spot.get().getSpotId();
@@ -61,7 +134,16 @@ public class ParkingLot {
         throw new Exception("No such vehicle found");
     }
 
+    /**
+     * Returns list of registration numbers for Vehicle's whose color matches the queried color
+     * @param color Color of the Vehicle
+     * @return List of registration numbers of Vehicles
+     * @throws Exception Throws exception if parking lot not created or no matching vehicles found
+     */
     public List<String> getRegistrationNumbersByColor(String color) throws Exception {
+
+        checkParkingLot();
+
         List<String> registrationNumbers = this.occupiedSpots.values().stream().filter(parkingSpot -> parkingSpot.getVehicle().getColor().equals(color)).map(parkingSpot -> parkingSpot.getVehicle().getRegistrationNumber()).collect(Collectors.toList());
         if(registrationNumbers.isEmpty()){
             throw new Exception("No such vehicles found");
@@ -69,24 +151,54 @@ public class ParkingLot {
         return registrationNumbers;
     }
 
-    public List<Integer> getSlotNumbersByColor(String color) throws Exception {
-        List<Integer> slotNumbers = this.occupiedSpots.entrySet().stream().filter( (entry) -> entry.getValue().getVehicle().getColor().equals(color)).map( entry -> entry.getKey()).collect(Collectors.toList());
+    /**
+     * Returns the SpotIDs where Vehicle of a specific color is parked.
+     * @param color Color of the vehicle
+     * @return List of SpotIDs
+     * @throws Exception Throws exception if parking lot not created, or no such spots found
+     */
+    public List<Integer> getSpotNumbersByColor(String color) throws Exception {
+
+        checkParkingLot();
+
+        List<Integer> slotNumbers = this.occupiedSpots.entrySet().stream().filter( (entry) -> entry.getValue().getVehicle().getColor().equals(color)).map(Map.Entry::getKey).collect(Collectors.toList());
         if(slotNumbers.isEmpty()){
-            throw new Exception("No such slots found");
+            throw new Exception("No such spots found");
         }
         return slotNumbers;
     }
 
-    private ParkingSpot getNextAvailableSpot() throws Exception {
+    /**
+     * Returns the next available SpotID from PriorityQueue.
+     * @return Next available SpotID
+     * @throws Exception Throws exception is parking lot not created, or parking lot full
+     */
+    public Integer getNextAvailableSpot() throws Exception {
+
+        checkParkingLot();
+
+        if(this.emptySpots.isEmpty()){
+            throw new Exception("Sorry, parking lot is full");
+        }
+        return this.emptySpots.peek();
+    }
+
+    private Integer useNextAvailableSpot() throws Exception {
+
+        checkParkingLot();
+
         if(this.emptySpots.isEmpty()){
             throw new Exception("Sorry, parking lot is full");
         }
         return this.emptySpots.poll();
     }
 
+    /**
+     * Adds a freed SpotID to the emptySpots PriorityQueue
+     * @param parkingSpot ParkingSpot that is to be freed
+     */
     private void freeSpot(ParkingSpot parkingSpot) {
-        this.emptySpots.add(parkingSpot);
+        this.emptySpots.add(parkingSpot.getSpotId());
     }
 
 }
-
